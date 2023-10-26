ec9aa785087f155b813b8d85ca7990a8c0318a01
Abdulla Alhammadi <abdulla.alhammadi29@gmail.com>
2023-10-26 23:52:39 +0400

    First commit

---
+++ automation.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import duckdb
+import string
+
+data = (pd.read_csv('merged_gw.csv')
+        .drop(columns = "kickoff_time"))
+
+indicators = pd.read_json('queries.json')
+
+with pd.ExcelWriter("FPL.xlsx", engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+        
+        for index, row in indicators.iterrows():
+                num = 0
+                for c in row["startcol"]:
+                        if c in string.ascii_letters:
+                                num = num * 26 + (ord(c.upper()) - ord('A'))
+                
+                result = (duckdb.sql(row["query"])
+                          .to_df())
+                
+                result.to_excel(writer, index=False,
+                                startcol=num,
+                                startrow=row["startrow"]-1,
+                                header=False)
