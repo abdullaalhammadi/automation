ad94c345e1619639bc104c95216e1f87b9f7ba58
Abdulla Alhammadi <abdulla.alhammadi29@gmail.com>
2023-10-27 14:22:08 +0400

    Fixed progression bar and added description of each iteration

--- automation.py
+++ automation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import duckdb
 import string
+from time import sleep
+from tqdm import tqdm
 
 data = (pd.read_csv('merged_gw.csv')
         .drop(columns = "kickoff_time"))
@@ -8,18 +10,24 @@ data = (pd.read_csv('merged_gw.csv')
 indicators = pd.read_json('queries.json')
 
 with pd.ExcelWriter("FPL.xlsx", engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-        
-        for index, row in indicators.iterrows():
+
+        iterator = tqdm(indicators.iterrows(), total=indicators.shape[0])
+
+        for index, row in iterator:
                 num = 0
                 for c in row["startcol"]:
                         if c in string.ascii_letters:
-                            num = (num * 26 + 1 + ord(c) - ord('A'))
-                num = num - 1
-                
+                                num = (num * 26 + 1 + ord(c) - ord('A'))
+  
                 result = (duckdb.sql(row["query"])
                           .to_df())
-                
                 result.to_excel(writer, index=False,
-                                startcol=num,
+                                startcol=num-1,
                                 startrow=row["startrow"]-1,
                                 header=False)
+                title = row["indicator_number"]
+
+                sleep(0.5)
+
+                iterator.set_description(f"Indicator {title} calculated successfully"
+                                         .format(title = row["indicator_number"]))
