b684665a9584f59282e6ec27009451e53260fa91
Abdulla Alhammadi <abdulla.alhammadi29@gmail.com>
2023-10-30 17:47:42 +0400

    Edited the query to read from the cell range

--- automation.py
+++ automation.py
@@ -2,7 +2,8 @@ import pandas as pd
 import duckdb
 import string
 from time import sleep
-from tqdm import tqdm
+from tqdm.auto import tqdm
+import re
 
 data = (pd.read_csv('merged_gw.csv')
         .drop(columns = "kickoff_time"))
@@ -10,24 +11,26 @@ data = (pd.read_csv('merged_gw.csv')
 indicators = pd.read_json('queries.json')
 
 with pd.ExcelWriter("FPL.xlsx", engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+        
+        for index, row in tqdm(indicators.iterrows(), total=indicators.shape[0]):
 
-        iterator = tqdm(indicators.iterrows(), total=indicators.shape[0])
+                starting_column = ''.join(re.findall(r'[A-Za-z]', row["range"].split(':')[0]))
+                
+                starting_row = int(''.join(re.findall(r'\d', row["range"].split(':')[0]))) - 1
 
-        for index, row in iterator:
                 num = 0
-                for c in row["startcol"]:
+                for c in starting_column:
                         if c in string.ascii_letters:
                                 num = (num * 26 + 1 + ord(c) - ord('A'))
-  
+                num = num - 1
                 result = (duckdb.sql(row["query"])
                           .to_df())
                 result.to_excel(writer, index=False,
-                                startcol=num-1,
-                                startrow=row["startrow"]-1,
+                                startcol=num,
+                                startrow=starting_row,
                                 header=False)
                 title = row["indicator_number"]
 
-                sleep(0.5)
+                sleep(2)
 
-                iterator.set_description(f"Indicator {title} calculated successfully"
-                                         .format(title = row["indicator_number"]))
+                print(f'Calculated indicator {title}'.format(title = title))
